b5370a9643c3ec98432f3b42af84280f97bd7f64
dev-marko <markospasenovski00@gmail.com>
2023-04-14 10:59:31 +0200

    Create File entity

---
+++ Models/File.cs
@@ -0,0 +1,10 @@
+﻿namespace ElevenCourses.Models;
+
+public class File
+{
+    public Guid Id { get; set; }
+    public string FilePath { get; set; }
+    public string FileUrl { get; set; }
+    public string CreatorId { get; set; }
+    public ApplicationUser Creator { get; set; }
+}
