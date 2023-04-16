d2fe8c6d68c952486c2d4a8769391431d85415ea
milanangelov <mileangelov1@gmail.com>
2023-04-17 00:45:05 +0200

    Created model for files with new name

---
+++ Models/PdfFile.cs
@@ -0,0 +1,12 @@
+﻿namespace ElevenCourses.Models;
+
+public class PdfFile
+{
+    public Guid Id { get; set; }
+    public string? FilePath { get; set; }
+    public string? FileUrl { get; set; }
+    public string? CreatorId { get; set; }
+    public ApplicationUser? Creator { get; set; }
+    public Guid WeekId { get; set; }
+    public Week? Week { get; set; }
+}
