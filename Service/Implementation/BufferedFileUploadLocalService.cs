d63caef25c55518a0d3e1414793832c232a7c7d4
dev-marko <markospasenovski00@gmail.com>
2023-04-24 21:26:56 +0200

    Minor refactor to BufferedFileUploadLocalService

--- Service/Implementation/BufferedFileUploadLocalService.cs
+++ Service/Implementation/BufferedFileUploadLocalService.cs
@@ -6,32 +6,28 @@ namespace ElevenCourses.Service.Implementation
     {
         public async Task<string> UploadFile(string folderPath, IFormFile file)
         {
-            string path = "";
+            var path = "";
             try
             {
-                if (file.Length > 0)
+                if (file.Length <= 0) return path;
+                
+                if (!Directory.Exists("CourseDocuments"))
                 {
-                    if (!Directory.Exists("weeks"))
-                    {
-                        Directory.CreateDirectory("weeks");
-                    }
-
-                    path = Path.GetFullPath(Path.Combine("weeks", folderPath));
-
-                    if (!Directory.Exists(path))
-                    {
-                        Directory.CreateDirectory(path);
-                    }
-                    using (var fileStream = new FileStream(Path.Combine(path, file.FileName), FileMode.Create))
-                    {
-                        await file.CopyToAsync(fileStream);
-                    }
-                    return path;
+                    Directory.CreateDirectory("CourseDocuments");
                 }
-                else
+
+                path = Path.GetFullPath(Path.Combine("CourseDocuments", folderPath));
+
+                if (!Directory.Exists(path))
                 {
-                    return path;
+                    Directory.CreateDirectory(path);
                 }
+
+                await using var fileStream = new FileStream(Path.Combine(path, file.FileName), FileMode.Create);
+                await file.CopyToAsync(fileStream);
+                
+                return path;
+
             }
             catch (Exception ex)
             {
