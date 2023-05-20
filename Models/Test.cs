6916caf017a881f6fa92fec9ced0a57ed9cc6bff
hristijan-stojanov <82612180+hristijan-stojanov@users.noreply.github.com>
2023-05-20 18:26:55 +0200

    add Test option

---
+++ Models/Test.cs
@@ -0,0 +1,19 @@
+﻿using ElevenCourses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Tests.Models
+{
+    public class Test
+    {
+        public Guid id { set; get; }
+        public string userId { set; get; }
+        public string name { set; get; }
+        public int time { set; get; }
+        public ApplicationUser user { set; get; }
+        public  ICollection<QuestionInTest> Questions { set; get; }
+
+    }
+}
