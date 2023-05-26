5d5807a9097059fc2d1ff02f8328b5085072022b
Verica100ilkova <verica.stoilkova@students.finki.ukim.mk>
2023-05-26 21:56:05 +0200

    My Courses Page Refactoring

--- Models/Course.cs
+++ Models/Course.cs
@@ -4,11 +4,10 @@ namespace ElevenCourses.Models;
 
 public class Course
 {
+    [DisplayName("Course")]
     public Guid Id { get; set; }
-
     [DisplayName("Course Name")]
     public string? Name { get; set; }
-    [DisplayName("Course")]
     public string? Description { get; set; }
     public string? CreatorId { get; set; }
     public ApplicationUser? Creator { get; set; }
